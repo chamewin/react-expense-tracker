4acc32feb6ffa12d275a67dd8801da402f8372ba
James Nguyen <jamesnguyen@Jamess-MacBook-Pro.local>
2022-06-02 15:35:48 -0400

    better way of displaying the correct expenses for the chosen filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,14 +7,14 @@ import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState(2022);
-  const [filteredExpenses, setFilteredExpenses] = useState(props.items);
 
   const filterSelectHandler = (selectedFilterYear) => {
     setFilterYear(selectedFilterYear);
-    setFilteredExpenses(props.items.filter((item) => {
-      return item.date.getFullYear().toString() === selectedFilterYear;
-    }));
   };
+
+  const filteredExpenses = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filterYear;
+  });
 
   return (
     <div>
