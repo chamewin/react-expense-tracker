a0ca7d11b8a60ef3b008f45fb96d32ad771f8a40
chamewin <29683200+chamewin@users.noreply.github.comnguyen.jmin@gmail.com>
2022-06-02 16:26:17 -0400

    created a ExpensesList component to handle rendering the list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import "./ExpenseItem.css"; 
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState(2022);
@@ -17,19 +17,6 @@ const Expenses = (props) => {
     return item.date.getFullYear().toString() === filterYear;
   });
 
-  let expensesContent = <p className="expense-item text-center">No expense was found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        date={expense.date}
-        amount={expense.amount}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
           filterYear={filterYear}
           onFilterSelect={filterSelectHandler}
         />
-        {expensesContent}
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
